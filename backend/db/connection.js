dbbd0595a757e8e589a57756bde4b237e0c0fa55
david2tosin <david2tosin@gmail.com>
2023-11-24 22:19:47 +0100

    commit

---
+++ backend/db/connection.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import "dotenv/config.js";
+
+// Define your connection URI here
+const MONGODB_URI =
+  "mongodb+srv://david2tosin:Sasukekun134@cluster0.wrzvfi0.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", false);
+mongoose.set("returnOriginal", false); //for findByAndUpdate to return a reference to object at location
+
+mongoose
+  .connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .catch((error) => {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+  });
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB has disconnected!");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error(`Error connecting to MongoDB: ${error}`);
+});
+
+export default mongoose.connection;
